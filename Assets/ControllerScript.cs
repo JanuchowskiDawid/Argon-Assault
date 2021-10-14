2c627ba0208627ad8f15821f27d933c3b0f54cf2
mmalpa <mmalpa97@wp.pl>
2021-10-14 22:30:56 +0200

    Trying new input system

--- Assets/ControllerScript.cs
+++ Assets/ControllerScript.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class ControllerScript : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+    void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    void OnDisable()
+    {
+        movement.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
         Debug.Log(horizontalThrow);
-        float verticalThrow = Input.GetAxis("Vertical");
+        float verticalThrow = movement.ReadValue<Vector2>().y;
         Debug.Log(verticalThrow);
 
     }
