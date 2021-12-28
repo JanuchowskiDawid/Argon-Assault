ede03751ade66dd39ebdb5473d8550039f54c5a6
JanuchowskiDawid <mmalpa97@wp.pl>
2021-12-28 21:03:41 +0100

    Lasers collisions improvement, now we can kill but also we can die by colliding with something

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 2f;
+
 
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log(this.name + "Collided with" + other.gameObject.name);
+        StartCrashSequence();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(this.name + "Trigged with" + other.name);
+        StartCrashSequence();
     }
+
+    void StartCrashSequence()
+    {
+        GetComponent<ControllerScript>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
 }
