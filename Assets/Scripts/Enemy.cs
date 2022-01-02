e9b6fe2450cfdc3afd690701572efcf5730cb95a
JanuchowskiDawid <mmalpa97@wp.pl>
2022-01-02 14:22:48 +0100

    Enemies destroys with particles, after delay destroy whole gameobject

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    [SerializeField] ParticleSystem explosion;
     private void OnParticleCollision(GameObject other)
     {
-        Destroy(this.gameObject);
+        explosion.Play();
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<SphereCollider>().enabled = false;
+        Destroy(gameObject, 2f);
     }
 }
