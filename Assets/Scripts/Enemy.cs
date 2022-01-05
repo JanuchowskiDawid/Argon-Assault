3deabf4924a8b1e4e0722e3920598630a904bc21
JanuchowskiDawid <mmalpa97@wp.pl>
2022-01-05 23:42:38 +0100

    Created new Scene to polish enemies prefabs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,17 +6,24 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] ParticleSystem explosion;
     [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 3;
     Score score;
     private void Start()
     {
         score = FindObjectOfType<Score>();
+
     }
     private void OnParticleCollision(GameObject other)
     {
-        explosion.Play();
+        hitPoints--;
         score.IcreaseScore(scorePerHit);
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<SphereCollider>().enabled = false;
-        Destroy(gameObject, 2f);
+        if (hitPoints==0)
+        {
+            explosion.Play();
+            score.IcreaseScore(scorePerHit);
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<SphereCollider>().enabled = false;
+            Destroy(gameObject, 2f);
+        }
     }
 }
