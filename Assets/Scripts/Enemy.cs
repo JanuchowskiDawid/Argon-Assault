089a74ede926a2b8ef61772f1c442dba4d6195c6
JanuchowskiDawid <mmalpa97@wp.pl>
2022-01-07 09:01:19 +0100

    Created parent to store all vfx

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] ParticleSystem explosion;
+    [SerializeField] GameObject explosion;
+    GameObject parentGameObject;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] int hitPoints = 3;
     Score score;
+
+    GameObject hitVFX;
     private void Start()
     {
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
         score = FindObjectOfType<Score>();
         Rigidbody rb = gameObject.AddComponent<Rigidbody>();
         rb.useGravity = false;
     }
     private void OnParticleCollision(GameObject other)
     {
+        hitVFX = explosion;
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
         hitPoints--;
         score.IcreaseScore(scorePerHit);
         if (hitPoints==0)
         {
-            explosion.Play();
+            vfx = Instantiate(explosion, transform.position, Quaternion.identity);
+            vfx.transform.parent = parentGameObject.transform;
             score.IcreaseScore(scorePerHit);
             GetComponent<MeshRenderer>().enabled = false;
             if (gameObject.GetComponent<SphereCollider>() != null)
