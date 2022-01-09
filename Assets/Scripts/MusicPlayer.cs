d39d569aa51979eeae7de0adce518e0f0f9404dd
JanuchowskiDawid <mmalpa97@wp.pl>
2022-01-09 12:08:48 +0100

    Added background music and script that keeps it alive

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        int numberOfMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        if (numberOfMusicPlayers>1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
